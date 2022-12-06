d019d65b9f3a7e74eb64ed8b5cea9ca324de8125
Stephan Lachnit <stephanlachnit@debian.org>
2022-12-06 13:37:42 +0100

    ST/MT RunManager Benchmark
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- src/DetectorConstruction.cpp
+++ src/DetectorConstruction.cpp
@@ -1,11 +1,56 @@
 #include "DetectorConstruction.hpp"
 
+#include <string>
+
 #include "G4SystemOfUnits.hh"
 #include "G4NistManager.hh"
 #include "G4Box.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4Transform3D.hh"
+#include "G4VPVParameterisation.hh"
+#include "G4PVParameterised.hh"
+
+template<typename T> void intentionallyUnused(const T&) { }
+
+class Parameterization2DG4 : public G4VPVParameterisation {
+public:
+    /**
+     * @brief Construct the parameterization for pixels
+     * @param div_x Number of divisions in the x-direction (y is automatically inferred)
+     * @param size_x Size of single element in x-direction
+     * @param size_y Size of single element in y-direction
+     * @param offset_x Offset of grid in the x-direction
+     * @param offset_y Offset of grid in the y-direction
+     * @param pos_z Position of the 2D parameterization in the z-axis
+     */
+    Parameterization2DG4(int div_x, double size_x, double size_y, double offset_x, double offset_y, double pos_z)
+      : div_x_(div_x), size_x_(size_x), size_y_(size_y), offset_x_(offset_x), offset_y_(offset_y), pos_z_(pos_z) {}
+
+    /**
+     * @brief Place the physical volume at the correct place with the copy number
+     * @param copy_id Id of the volume on the grid
+     * @param phys_volume Physical volume to place on the grid
+     */
+    void ComputeTransformation(const G4int copy_id, G4VPhysicalVolume* phys_volume) const override {
+        auto idx_x = copy_id % div_x_;
+        auto idx_y = copy_id / div_x_;
+
+        auto pos_x = (idx_x + 0.5) * size_x_ + offset_x_;
+        auto pos_y = (idx_y + 0.5) * size_y_ + offset_y_;
+
+        phys_volume->SetTranslation(G4ThreeVector(pos_x, pos_y, pos_z_));
+        phys_volume->SetRotation(nullptr);
+    }
+
+private:
+    int div_x_;
+    double size_x_;
+    double size_y_;
+    double offset_x_;
+    double offset_y_;
+    double pos_z_;
+};
 
 G4VPhysicalVolume* DetectorConstruction::Construct() {
     auto* nist = G4NistManager::Instance();
@@ -16,6 +61,34 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     auto* solidWorld = new G4Box("World", worldHalfSize, worldHalfSize, worldHalfSize);
     auto* logicWorld = new G4LogicalVolume(solidWorld, worldMaterial, "World");
     auto* physiWolrd = new G4PVPlacement(G4Transform3D(), logicWorld, "World", nullptr, false, 0, fCheckOverlaps);
+
+    // Pixel Detector
+    double pixelDetectorSizeXY = 2. * cm;
+    double pixelDetectorSizeZ = 100. * um;
+    auto* pixelDetectorMaterial = nist->FindOrBuildMaterial("G4_Si");
+    int pixelsNumXY = 1000;
+    double pixelsSizeXY = 20. * um;
+
+    // Single Pixel Cell (for simplicity only Si without inner structure)
+    auto* solidSinglePixel = new G4Box("SinglePixel", 0.5 * pixelsSizeXY, 0.5 * pixelsSizeXY, 0.5 * pixelDetectorSizeZ);
+    auto* logicSinglePixel = new G4LogicalVolume(solidSinglePixel, pixelDetectorMaterial, "SinglePixel");
+
+    // Pixel Matrix
+    auto* paramPixels = new Parameterization2DG4(pixelsNumXY, pixelsSizeXY, pixelsSizeXY, -0.5 * pixelDetectorSizeXY, -0.5 * pixelDetectorSizeXY, 0.);
+
+    for (size_t n = 0; n < 5; ++n) {
+        // Place Outer Pixel Detector Volume
+        std::string pixelDetectorName = "PixelDetector" + std::to_string(n);
+        auto* solidPixelDetector = new G4Box(pixelDetectorName, 0.5 * pixelDetectorSizeXY, 0.5 * pixelDetectorSizeXY, 0.5 * pixelDetectorSizeZ);
+        auto* logicPixelDetector = new G4LogicalVolume(solidPixelDetector, pixelDetectorMaterial, pixelDetectorName);
+        auto* physiPixelDetector = new G4PVPlacement(G4Transform3D({}, {0., 0., n * cm - 0.4 * m}), logicPixelDetector, pixelDetectorName, logicWorld, false, 0, fCheckOverlaps);
+        intentionallyUnused(physiPixelDetector);
+
+        // Place Pixels
+        std::string pixelsName = "Pixels" + std::to_string(n);
+        auto* physiPixels = new G4PVParameterised(pixelsName, logicSinglePixel, logicPixelDetector, EAxis::kUndefined, pixelsNumXY * pixelsNumXY, paramPixels, false);
+        intentionallyUnused(physiPixels);
+    }
 
     return physiWolrd;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <chrono>
+#include <iostream>
+
 #include "G4UIExecutive.hh"
-#include "G4RunManagerFactory.hh"
+#include "G4RunManager.hh"
+#include "G4MTRunManager.hh"
 #include "FTFP_BERT.hh"
 #include "G4VisExecutive.hh"
 #include "G4UImanager.hh"
@@ -8,34 +12,91 @@
 #include "DetectorConstruction.hpp"
 #include "ActionInitialization.hpp"
 
-int main(int argc, char** argv) {
-
-    G4UIExecutive* ui = nullptr;
-    if (argc == 1) {
-        ui = new G4UIExecutive(argc, argv);
+G4RunManager* prepRunManager(bool MT) {
+    G4RunManager* runManager = nullptr;
+    if (MT) {
+        runManager = new G4MTRunManager();
+        runManager->SetNumberOfThreads(1);
     }
-
-    auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+    else {
+        runManager = new G4RunManager();
+    }
 
     runManager->SetUserInitialization(new DetectorConstruction());
     runManager->SetUserInitialization(new FTFP_BERT());
     runManager->SetUserInitialization(new ActionInitialization());
 
+    // Ensure proper initialization
+    runManager->Initialize();
+    runManager->BeamOn(0);
+
+    return runManager;
+}
+
+std::chrono::milliseconds bench(G4RunManager* runManager, size_t runs) {
+    auto start = std::chrono::steady_clock::now();
+    for (size_t n = 0; n < runs; ++n) {
+        runManager->BeamOn(1);
+    }
+    auto stop = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+}
+
+int vis(int argc, char** argv) {
+    auto* ui = new G4UIExecutive(argc, argv);
+    auto* runManager = prepRunManager(true);
+
     auto* visManager = new G4VisExecutive();
     visManager->Initialize();
     auto* UImanager = G4UImanager::GetUIpointer();
 
-    if (!ui) {
-        G4String cmd = "/control/execute ";
-        UImanager->ApplyCommand(cmd + argv[1]);
-    }
-    else {
-        ui->SessionStart();
-    }
+    G4String vis_backend = (argc == 3) ? argv[2] : "OGL";
+    UImanager->ApplyCommand("/run/initialize");
+    UImanager->ApplyCommand("/vis/open " + vis_backend);
+    UImanager->ApplyCommand("/vis/drawVolume");
+    UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
+    UImanager->ApplyCommand("/vis/modeling/trajectories/create/drawByCharge");
+    ui->SessionStart();
 
     delete ui;
     delete visManager;
     delete runManager;
 
     return 0;
+}
+
+int errorMessage() {
+    std::cerr << "Usage: [st, mt, vis [vis_backend]]" << std::endl;
+    return 1;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        return errorMessage();
+    }
+    if (strcmp(argv[1], "vis") == 0) {
+        return vis(argc, argv);
+    }
+
+    if (strcmp(argv[1], "mt") == 0) {
+        std::cout << "Preparing MT Benchmark..." << std::endl;
+        auto* runManagerMT = prepRunManager(true);
+        std::cout << "Starting MT benchmark..." << std::endl;
+        auto result_mt = bench(runManagerMT, 1000);
+        std::cout << "Result MT " << result_mt.count() << " ms" << std::endl;
+        delete runManagerMT;
+        return 0;
+    }
+
+    if (strcmp(argv[1], "st") == 0) {
+        std::cout << "Preparing ST Benchmark..." << std::endl;
+        auto* runManagerST = prepRunManager(false);
+        std::cout << "Starting ST benchmark..." << std::endl;
+        auto result_st = bench(runManagerST, 1000);
+        std::cout << "Result ST " << result_st.count() << " ms" << std::endl;
+        delete runManagerST;
+        return 0;
+    }
+
+    return errorMessage();
 }
